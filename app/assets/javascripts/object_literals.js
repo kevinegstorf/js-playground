9807824c5ab0ae17c292c9d6f49014da2ead7763
kevinegstorf <kevin.egstorf@gmail.com>
2016-06-26 21:15:54 +0200

    created object literal js playground

---
+++ app/assets/javascripts/object_literals.js
@@ -0,0 +1,17 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+var hotel = {
+  name: "L'europe",
+  rooms: 100,
+  booked: 25,
+  checkAvailability: function() {
+    return this.rooms - this.booked;
+  }
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
---
+++ app/helpers/object_literals_helper.rb
@@ -0,0 +1,2 @@
+module ObjectLiteralsHelper
+end
